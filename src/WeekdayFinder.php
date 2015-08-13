e4ac0dbe78eb81fa1bd7c46236d91547968d2747
Logan <student@epicodus.com>
2015-08-13 16:44:46 -0700

    fix no-year error

--- src/WeekdayFinder.php
+++ src/WeekdayFinder.php
@@ -4,8 +4,10 @@
         function findWeekday($month, $day, $year)
         {
             date_default_timezone_set('UTC');
-            
-            if (!checkdate($month, $day, $year)) {
+
+            if (empty($year)) {
+                return "Please enter a year.";
+            } elseif (!checkdate($month, $day, $year)) {
                 return "Invalid date!";
             } else {
                 return date("l", mktime(0, 0, 0, $month, $day, $year));
